d5fc646848217c6f32940f7bf7a64cd02b90686a
KendrickChou <847361724@qq.com>
2023-12-28 18:07:29 +0800

    fix user setting override

--- lua/user/settings.lua
+++ lua/user/settings.lua
@@ -10,51 +10,57 @@ settings["background"] = "dark"
 
 settings["use_copilot"] = false
 
-settings["lsp_deps"] = {
-	-- "bashls",
-	"clangd",
-	-- "html",
-	-- "jsonls",
-	-- "lua_ls",
-	-- "pylsp",
-	-- "gopls",
-	-- "texlab",
-	"rust_analyzer",
-}
+settings["lsp_deps"] = function(defaults)
+	return {
+		-- "bashls",
+		"clangd",
+		-- "html",
+		-- "jsonls",
+		-- "lua_ls",
+		-- "pylsp",
+		-- "gopls",
+		-- "texlab",
+		"rust_analyzer",
+	}
+end
 
-settings["treesitter_deps"] = {
-	"bash",
-	"c",
-	"cpp",
-	-- "css",
-	-- "go",
-	-- "gomod",
-	-- "html",
-	-- "javascript",
-	-- "json",
-	-- "latex",
-	-- "lua",
-	"make",
-	-- "markdown",
-	-- "markdown_inline",
-	"python",
-	"rust",
-	-- "typescript",
-	-- "vimdoc",
-	-- "vue",
-	-- "yaml",
-}
+settings["treesitter_deps"] = function(defaults)
+	return {
+		"bash",
+		"c",
+		"cpp",
+		-- "css",
+		-- "go",
+		-- "gomod",
+		-- "html",
+		-- "javascript",
+		-- "json",
+		-- "latex",
+		-- "lua",
+		"make",
+		-- "markdown",
+		-- "markdown_inline",
+		"python",
+		"rust",
+		-- "typescript",
+		-- "vimdoc",
+		-- "vue",
+		-- "yaml",
+	}
+end
 
-settings["null_ls_deps"] = {
-	"clang_format",
-	-- "gofumpt",
-	-- "goimports",
-	"prettier",
-	"shfmt",
-	"stylua",
-	"vint",
-	-- "rustfmt",
-}
+settings["null_ls_deps"] = function(defaults)
+	return {
+		"clang_format",
+		-- "gofumpt",
+		-- "goimports",
+		"prettier",
+		"shfmt",
+		"stylua",
+		"vint",
+		-- "rustfmt",
+	}
+end
 
 settings["formatter_block_list"] = {
 	markdown = true,
