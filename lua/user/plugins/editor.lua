915d5a81b6c25a96fc961f388b0db38f8dee75df
KendrickChou <847361724@qq.com>
2024-03-14 12:46:44 +0800

    minor

--- lua/user/plugins/editor.lua
+++ lua/user/plugins/editor.lua
@@ -15,7 +15,7 @@ custom["lervag/vimtex"] = {
 	config = function()
 		vim.g.vimtex_view_method = "skim"
 		vim.g.vimtex_compiler_latexmk_engines = {
-			_ = "-pdflatex",
+			_ = "-xelatex",
 		}
 		vim.g.vimtex_view_skim_sync = 1
 		-- vim.g.vimtex_view_skim_activate = 1
